5c9006922543b7bfcaeedce49cfab8c855028f23
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-04-12 19:55:53 +0530

    Create setup.sh

--- setup.sh
+++ setup.sh
@@ -5,34 +5,44 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! DUDE PLEASE ENTER NGROK !!! \e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m          !!! DUDE PLEASE ENTER NGROK !!! \e[0m'
 sleep 2
 clear
 echo ""
 echo ""
-echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
-echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
-echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo -e $'\e[1;36m                              __ \e[0m'
+echo -e $'\e[1;36m          ____  _            |  |        __  __ _   _  ____ \e[0m'
+echo -e $'\e[1;36m         / ___|(_)_ __    ___|  |___    |  \/  | \ | |/ ___| \e[0m'
+echo -e $'\e[1;31m         \___ \| | `__|   \__    __/    | |\/| |  \| |\___ \ \e[0m'
+echo -e $'\e[1;31m          ___) | | |         \  /       | |  | | |\  | ___) | \e[0m'
+echo -e $'\e[1;31m  __  __ |____/|_|_| _     _  \/ _  __  |_|  |_|_| \_||____/ \e[0m'
+echo -e $'\e[1;31m |  \/  | __ _ _ __ (_)___| |__ | |/ /   _ _ __ ___   __ _ _ _ \e[0m'
+echo -e $'\e[1;37m | |\/| |/ _` | `_ \| / __| `_ \| ` / | | | `_ ` _ \ / _` | `_| \e[0m'
+echo -e $'\e[1;37m | |  | | (_| | | | | \__ \ | | | . \ |_| | | | | | | (_| | | \e[0m'
+echo -e $'\e[1;32m |_|  |_|\__,_|_| |_|_|___/_| |_|_|\_\__,_|_| |_| |_|\__,_|_| \e[0m'
+echo -e $'\e[1;32m╭━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╮ \e[0m'
+echo -e $'\e[1;31m   Created By:- https://github.com/SirManishKumar/MNS-Ngrok  \e[0m'
+echo -e $'\e[1;31m     Follow On Twitter:- http://twtter.com/SirManishKumar  \e[0m'
+echo -e $'\e[1;32m╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯ \e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m        ----------------------------------------- \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m       !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m        ----------------------------------------- \e[1;91m\e[0m'
 echo ""
 termux-setup-storage
 apt install
 apt update -y
 apt upgrade -y
-apt install ruby -y
-apt install php -y
-apt install jq -y
-apt install tail -y
-apt install curl -y
 apt install zip -y
 pkg install wget -y
-pkg install ruby -y
-pkg install gem -y
-gem install lolcat
 clear
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
+mv ngrok-stable-linux-arm.zip $HOME
 unzip ngrok-stable-linux-arm.zip
-
+rm -rf ngrok-stable-linux-arm.zip
+clear
+echo ""
 echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m KEEP IN MIND YOU HAVE TO ENTER  \e[1;33m[ *** ]\e[0m'
+echo ""
 read -p $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
 $token
 sleep2
