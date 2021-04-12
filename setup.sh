d890140fb2b285ba2c2cfd93e979e36a71239b5d
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-04-12 22:06:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ echo ""
 clear
 echo ""
 echo ""
-echo -e $'\e[1;36m                              __ \e[0m'                                                                       echo -e $'\e[1;36m          ____  _            |  |        __  __ _   _  ____ \e[0m'
+echo -e $'\e[1;36m                              __ \e[0m'
+echo -e $'\e[1;36m          ____  _            |  |        __  __ _   _  ____ \e[0m'
 echo -e $'\e[1;36m         / ___|(_)_ __    ___|  |___    |  \/  | \ | |/ ___| \e[0m'
 echo -e $'\e[1;31m         \___ \| | `__|   \__    __/    | |\/| |  \| |\___ \ \e[0m'
 echo -e $'\e[1;31m          ___) | | |         \  /       | |  | | |\  | ___) | \e[0m'
@@ -117,7 +118,7 @@ echo -e $'\e[1;31m   Created By:- https://github.com/SirManishKumar/MNS-Ngrok  \
 echo -e $'\e[1;31m     Follow On Twitter:- http://twtter.com/SirManishKumar  \e[0m'
 echo -e $'\e[1;32m╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯ \e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m           --------------------------------- \e[1;91m\e[0m'
-echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m      !!!    Installation Successfull   !!!\e[1;91m\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m      !!!    Installation Successfull   !!!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m           --------------------------------- \e[1;91m\e[0m'
 sleep 4
 clear
