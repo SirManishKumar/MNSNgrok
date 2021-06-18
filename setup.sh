bc850c2c7c873df634fd56a541a8e36ffe53c774
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-06-18 10:46:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -86,6 +86,10 @@ echo -e "\e[1;37m                 Connecting- SirmANISHKUMAR"
 sleep .8
 clear
 echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirMANISHKUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
 echo -e "\e[1;37m                 Connecting- SirMaNISHKUMAR"
 sleep .8
 clear
@@ -107,6 +111,10 @@ sleep .8
 clear
 echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
 echo -e "\e[1;37m                 Connecting- SirManishkUMAR"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                 Connecting- SirManishKUMAR"
 sleep .8
 clear
 echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
